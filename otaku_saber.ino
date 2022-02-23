fcf675c1f9694f1beb42c5e260afa661a22655e6
uribo <ur1b0.dev@gmail.com>
2022-02-24 00:00:52 +0900

    neopixelの制御を追加

--- otaku_saber.ino
+++ otaku_saber.ino
@@ -1,9 +1,44 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
 
+#define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+#define NUMPIXELS 1 // Popular NeoPixel ring size
+#define DELAYVAL 50 // Time (in milliseconds) to pause between pixels
+
+static int flg_dim = 1;
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  Serial.begin(9600);      // 9600bpsでシリアルポートを開く
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  static int color_r = 0;
+  pixels.clear(); // Set all pixel colors to 'off'
 
+  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+    pixels.setPixelColor(i, pixels.Color(0, color_r, 0));
+    pixels.show();   // Send the updated pixel colors to the hardware.
+    delay(DELAYVAL); // Pause before next pass through loop
+  }
+
+  if(flg_dim){
+    color_r += 1;  
+    if(color_r > 50){
+      flg_dim = 0;
+    }
+  }else{
+    color_r -= 1;  
+    if(color_r < 1){
+      flg_dim = 1;
+    }
+  }
+  Serial.println(color_r);
 }
