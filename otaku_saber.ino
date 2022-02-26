f3806408dcc9b17c6ade817e7fe5aa5ab83e6bfe
uribo <ur1b0.dev@gmail.com>
2022-02-26 16:56:35 +0900

    スイッチを追加

--- otaku_saber.ino
+++ otaku_saber.ino
@@ -4,6 +4,11 @@
 #endif
 
 #define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+#define PIN_SW_L   2
+#define PIN_SW_C   1
+#define PIN_SW_R   0
+#define PIN_SW_A   4
+#define PIN_SW_B   3
 #define NUMPIXELS 1 // Popular NeoPixel ring size
 #define DELAYVAL 50 // Time (in milliseconds) to pause between pixels
 
@@ -15,12 +20,24 @@ void setup() {
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
 #endif
+  pinMode(PIN_SW_L, INPUT);
+  pinMode(PIN_SW_C, INPUT);
+  pinMode(PIN_SW_R, INPUT);
+  pinMode(PIN_SW_A, INPUT);
+  pinMode(PIN_SW_B, INPUT);
   Serial.begin(9600);      // 9600bpsでシリアルポートを開く
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 }
 
 void loop() {
   static int color_r = 0;
+  int tmp = 0;
+  tmp |= !digitalRead(PIN_SW_L);
+  tmp |= !digitalRead(PIN_SW_C);
+  tmp |= !digitalRead(PIN_SW_R);
+  tmp |= !digitalRead(PIN_SW_A);
+  tmp |= !digitalRead(PIN_SW_B);
+
   pixels.clear(); // Set all pixel colors to 'off'
 
   for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
@@ -40,5 +57,19 @@ void loop() {
       flg_dim = 1;
     }
   }
-  Serial.println(color_r);
+
+  if(tmp){
+    color_r = 0;
+  }
+
+  Serial.print("L:");
+  Serial.print(digitalRead(PIN_SW_L));
+  Serial.print(",C:");
+  Serial.print(digitalRead(PIN_SW_C));
+  Serial.print(",R:");
+  Serial.print(digitalRead(PIN_SW_R));
+  Serial.print(",A:");
+  Serial.print(digitalRead(PIN_SW_A));
+  Serial.print(",B:");
+  Serial.println(digitalRead(PIN_SW_B));
 }
