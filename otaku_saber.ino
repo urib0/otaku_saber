0461c00afaf8190a0f1bf1c33c5282f03984f967
uribo <ur1b0.dev@gmail.com>
2022-03-02 16:13:22 +0900

    スイッチ検知処理を追加

--- otaku_saber.ino
+++ otaku_saber.ino
@@ -20,6 +20,20 @@ ul time;
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN_DO, NEO_GRB + NEO_KHZ800);
 
+uc switch_detection(){
+  uc tmp = 0;
+
+  tmp = !digitalRead(PIN_SW_B);
+  tmp |= (!digitalRead(PIN_SW_A)) << 1;
+  tmp |= (!digitalRead(PIN_SW_R)) << 2;
+  tmp |= (!digitalRead(PIN_SW_C)) << 3;
+  tmp |= (!digitalRead(PIN_SW_L)) << 4;
+
+  Serial.println(tmp);
+
+  return tmp;
+}
+
 void setup() {
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
@@ -31,6 +45,9 @@ void setup() {
   pinMode(PIN_SW_B, INPUT);
   Serial.begin(9600);      // 9600bpsでシリアルポートを開く
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  delay(10);
+  pixels.setPixelColor(0, pixels.Color(0, 10, 0));
+  pixels.show();
 }
 
 void loop() {
@@ -38,6 +55,8 @@ void loop() {
 
   // 処理時間計測開始
   time = millis();
+
+  switch_detection();
 
   // 処理時間表示
   Serial.println(time);
